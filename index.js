ee11ab433c9699d198ad1bc598c90162fd0bb531
meil <ikonopistsev@roboforex.com>
2023-03-08 08:27:10 +0200

    interface

--- index.js
+++ index.js
@@ -1,26 +1,47 @@
 const EventEmitter = require("node:events");
 const os = require("os");
 
-let nativePath;
+let modulePath = './build/Release/node-stomptalk-native';
 if (os.type() == 'Windows_NT') {
     nativePath = './build/RelWithDebInfo/node-stomptalk-native';
-} else {
-    nativePath = './build/Release/node-stomptalk-native';
 };
 
-const { NativeStomptalk } = require(nativePath);
+const { NativeStomptalk } = require(modulePath);
 
 class StompTalk extends EventEmitter {
     constructor() {
         super();
-        const nativeStompTalk = new NativeStomptalk(19, (text) => {
-            console.log("onFrame:", text);
-        });
-        nativeStompTalk.plusOne((text)=>{
-            console.log("callback:", text);
-        });
-        nativeStompTalk.onFrame();
+        this.nativeStompTalk = new NativeStomptalk();
+    }
+
+    parse(data) {
+        return this.nativeStompTalk.parse(data,
+            err => {
+                this.emit('error', err);
+            },
+            () => {
+                this.emit('frameStart');
+            },
+            methodName => {
+                this.emit('method', methodName);
+            },
+            headerKey => {
+                this.emit('headerKey', headerKey);
+            },
+            headerVal => {
+                this.emit('headerVal', headerVal);
+            },
+            body => {
+                this.emit('body', body);
+            },
+            () => {
+                this.emit('frameEnd');
+            });
     }
 }
 
-new StompTalk();
+const stompTalk = new StompTalk();
+const rc = stompTalk.parse((value) => {
+    console.log("cb: ", value);
+})
+console.log("rc: ", rc);
--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,11 @@
 
 using namespace Napi;
 
-class NativeStomptalk final
-	: public ObjectWrap<NativeStomptalk>
-	, public stomptalk::hook_base
+struct NativeHook final
+	: stomptalk::hook_base
 {
+
+private:
 	virtual void on_frame(stomptalk::parser_hook&, const char *frame_start) noexcept override 
 	{	}
  
@@ -17,51 +18,29 @@ class NativeStomptalk final
  
     virtual void on_hdr_key(stomptalk::parser_hook&, 
         std::uint64_t hader_id, const char*, std::size_t) noexcept override
-	{
-
-	}
+	{   }
  
     virtual void on_hdr_val(stomptalk::parser_hook&, const char*, std::size_t) noexcept override
-	{
-
-	}
+	{   }
  
     virtual void on_body(stomptalk::parser_hook&, const void*, std::size_t) noexcept override
-	{
-
-	}
+	{   }
  
     virtual void on_frame_end(stomptalk::parser_hook&, const char *frame_end) noexcept override
-	{
+	{   }
+};
 
-	}
-
-	double value_;
-	Napi::Function onFrame_{};
-	// Napi::Function onMethod_{};
-	// Napi::Function onHeaderKey_{};
-	// Napi::Function onHeaderVal_{};
-	// Napi::Function onFrameEnd_{};
-
-		
+class NativeStomptalk final
+	: public ObjectWrap<NativeStomptalk>
+{	
 public:
     NativeStomptalk(const Napi::CallbackInfo& callbackInfo)
 		: ObjectWrap(callbackInfo)
-		, value_(callbackInfo[0].As<Napi::Number>().DoubleValue())
-		, onFrame_(callbackInfo[1].As<Napi::Function>())
-		// , onMethod_(callbackInfo[2].As<Napi::Function>())
-		// , onHeaderKey_(callbackInfo[3].As<Napi::Function>())
-		// , onHeaderVal_(callbackInfo[4].As<Napi::Function>())
-		// , onFrameEnd_(callbackInfo[5].As<Napi::Function>())
-	{   
-
-	}
+	{   }
    
 	static Napi::Object Init(Napi::Env env, Napi::Object exports) {
   		Napi::Function func = DefineClass(env, "NativeStomptalk", {
-			InstanceMethod("plusOne", &NativeStomptalk::PlusOne),
-            InstanceMethod("value", &NativeStomptalk::GetValue),
-			InstanceMethod("onFrame", &NativeStomptalk::onFrame),
+			InstanceMethod("parse", &NativeStomptalk::parse)
 		});
 
 		Napi::FunctionReference* constructor = new Napi::FunctionReference();
@@ -76,24 +55,15 @@ public:
 	{   }
 
 private:
-	Napi::Value PlusOne(const Napi::CallbackInfo& info) {
+	Napi::Value parse(const Napi::CallbackInfo& info) {
 		Napi::Env env = info.Env();
 		Napi::Function cb = info[0].As<Napi::Function>();
-		this->value_ = this->value_ + 1;
-  		cb.Call(env.Global(), {NativeStomptalk::GetValue(info)});
-		return NativeStomptalk::GetValue(info);
+  		cb.Call(env.Global(), { NativeStomptalk::GetValue(info) });
+		return Napi::Number::New(info.Env(), 0);
 	}
 
 	Napi::Value GetValue(const Napi::CallbackInfo& info) {
- 	 double num = this->value_;
-
-  		return Napi::Number::New(info.Env(), num);
-	}
-
-	Napi::Value onFrame(const Napi::CallbackInfo& info) {
-		Napi::Env env = info.Env();
-  		onFrame_.Call(env.Global(), {NativeStomptalk::GetValue(info)});
-		return NativeStomptalk::GetValue(info);
+  		return Napi::Number::New(info.Env(), 42);
 	}
 };
 
